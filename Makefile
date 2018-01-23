0fcac24b5a707fa60fdfced4c7ee09ac3d0e99cf
initbar <initbar@protonmail.ch>
2018-01-22 23:56:35 -0500

    move travis base build to rust

--- Makefile
+++ Makefile
@@ -11,3 +11,10 @@ test:
 	cd $(TESTS) && \
 	   pytest --benchmark-enable test_py_regex.py && \
 	   pytest --benchmark-enable test_py_replace.py
+
+clean:
+	rm -rfv $(SOURCE)/target
+	find $(SOURCE) \
+	     -type f \
+	     -iname "*.py[pc]" \
+	     -exec rm -fv "{}" \;
--- src/src/lib.rs
+++ src/src/lib.rs
@@ -1,6 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+#[macro_use]
+extern crate cpython;
+use cpython::{Python, PyResult};
+
+fn rust_print(_py: Python, text: &str) -> PyResult<u64> {
+    println!("{}", text);
+    Ok(0)
 }
+
+py_module_initializer!(libblazelib, initlibblazelib, PyInit_blazelib, |py, m| {
+    try!(m.add(py, "__doc__", "Blaze string manipulation library in Rust."));
+    try!(m.add(py, "rust_print", py_fn!(py, rust_print(text: &str))));
+    Ok(())
+});
