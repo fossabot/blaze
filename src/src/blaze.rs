d2e0b34404ac291889741a84174712b7e73ade5c
initbar <initbar@protonmail.ch>
2018-01-23 05:38:13 -0500

    extremely basic implementation

--- src/src/blaze.rs
+++ src/src/blaze.rs
@@ -4,33 +4,44 @@ extern crate cpython;
 
 use cpython::{Python, PyResult, PyObject};
 
-/** replace(pattern, repl, text) -- search and replace a `pattern` with `repl`
- *                                 inside given text.
+/** replace(pattern, repl, text)
+ * : search and replace a `pattern` with `repl` inside given text.
  * + {str} pattern -- absolute string.
  * + {str} repl -- replacement string.
  * + {str} text -- input string.
  */
-fn replace(py: Python,
-           pattern: &str,
-           repl: &str,
-           text: &str) -> PyResult<PyObject> {
-    Ok(py.None())
+pub fn replace(py: Python,
+               pattern: &str,
+               repl: &str,
+               text: &str) -> PyResult<String> {
+    let mut ret = String::new();
+    for character in text.chars() {
+        match character {
+            pattern => ret.push_str(repl),
+            _ => ret.push(character)
+        }
+    }
+    return Ok(ret)
 }
 
-/** to_lower(text) -- transform text to lowercase.
+/** to_lower(text)
+ * : transform text to lowercase.
  * + {str} text -- input string.
  */
-fn to_lower(py: Python,
-            text: &str) -> PyResult<PyObject> {
-    Ok(py.None())
+pub fn to_lower(py: Python,
+                text: &str) -> PyResult<String> {
+    let mut _text = text.to_string().to_lowercase();
+    return Ok(_text)
 }
 
-/** to_upper(text) -- transform text to uppercase.
+/** to_upper(text)
+ * : transform text to uppercase.
  * + {str} text -- input string.
  */
-fn to_upper(py: Python,
-            text: &str) -> PyResult<PyObject> {
-    Ok(py.None())
+pub fn to_upper(py: Python,
+                text: &str) -> PyResult<String> {
+    let mut _text = text.to_string().to_uppercase();
+    return Ok(_text)
 }
 
 //
@@ -40,7 +51,7 @@ fn to_upper(py: Python,
 py_module_initializer!(blaze, initblaze, PyInit_blaze, |py, m| {
     m.add(py, "__doc__", "blazingly-fast text manipulation engine at a quantum level.")?;
     m.add(py, "__name__", "blaze")?;
-    m.add(py, "replace", py_fn!(py, replace(text: &str,
+    m.add(py, "replace", py_fn!(py, replace(pattern: &str,
                                             repl: &str,
                                             text: &str)))?;
     m.add(py, "to_lower", py_fn!(py, to_lower(text: &str)))?;
