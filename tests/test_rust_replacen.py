106f8da5065adf260a3acf1076902f5795e8848c
initbar <initbar@protonmail.ch>
2018-01-31 18:08:47 -0500

    fix rust replacen tests

--- tests/test_rust_replacen.py
+++ tests/test_rust_replacen.py
@@ -12,38 +12,44 @@ def test_str_replace_10(benchmark):
     sample = generate_random_sample()
     benchmark(blaze.replacen,
               sample,
-              replace_terms)
+              replace_terms.keys(),
+              replace_terms.values())
 
 def test_str_replace_100(benchmark):
     sample = generate_random_sample(length=100)
     benchmark(blaze.replacen,
               sample,
-              replace_terms)
+              replace_terms.keys(),
+              replace_terms.values())
 
 # 1,000
 def test_str_replace_1000(benchmark):
     sample = generate_random_sample(length=1000)
     benchmark(blaze.replacen,
               sample,
-              replace_terms)
+              replace_terms.keys(),
+              replace_terms.values())
 
 # 10,000
 def test_str_replace_10000(benchmark):
     sample = generate_random_sample(length=10000)
     benchmark(blaze.replacen,
               sample,
-              replace_terms)
+              replace_terms.keys(),
+              replace_terms.values())
 
 # 100,000
 def test_str_replace_100000(benchmark):
     sample = generate_random_sample(length=100000)
     benchmark(blaze.replacen,
               sample,
-              replace_terms)
+              replace_terms.keys(),
+              replace_terms.values())
 
 # 1,000,000
 def test_str_replace_1000000(benchmark):
     sample = generate_random_sample(length=1000000)
     benchmark(blaze.replacen,
               sample,
-              replace_terms)
+              replace_terms.keys(),
+              replace_terms.values())
